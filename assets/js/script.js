29bb913c070b24f7b931987a27d2d1ea95601aa2
Victor Perez <zunaty@gmail.com>
2021-07-14 16:19:26 -0600

    updated time block layout

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var today = moment();
+
+// Save button is clicked
+$(".savebtn").click(function(){
+    console.log("hello");
+});
+
+$(".taskZone").click(function(){
+    $(".taskZone").append("<textarea>")
+    console.log("bye")
+});
