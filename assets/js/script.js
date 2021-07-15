7349fadb344f78c78a1491778a40f1e07966fde7
Victor Perez <zunaty@gmail.com>
2021-07-14 18:26:59 -0600

    textarea update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
-var today = moment();
+var currentTime = moment().format("H")
+var today = moment().format("LL");
+document.getElementById("currentDay").innerText = today;
+console.log(today);
+
+var hourUpdate = function() {
+    if(timeBlock < currentTime){
+        // all timeblocks will be gray
+    } else if(timeBlock == currentTime){
+        // timeblock will be green
+
+    } else if(timeBlock > currentTime){
+        // time block will be blue
+
+    } else {
+        // all timeblocks will be gray background
+    }
+    console.log(currentTime);
+};
 
 // Save button is clicked
 $(".savebtn").click(function(){
     console.log("hello");
 });
 
-$(".taskZone").click(function(){
-    $(".taskZone").append("<textarea>")
-    console.log("bye")
-});
+hourUpdate();
