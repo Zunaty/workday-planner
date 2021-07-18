c8a28789accf9c674fc64849f22a5d6b8e017598
Victor Perez <zunaty@gmail.com>
2021-07-18 14:08:26 -0600

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,49 @@
+var tasks = [];
 var currentTime = moment().format("H")
 var today = moment().format("LL");
 document.getElementById("currentDay").innerText = today;
 console.log(today);
+console.log(currentTime);
 
 var hourUpdate = function() {
-    if(timeBlock < currentTime){
-        // all timeblocks will be gray
-    } else if(timeBlock == currentTime){
-        // timeblock will be green
-
-    } else if(timeBlock > currentTime){
-        // time block will be blue
-
-    } else {
-        // all timeblocks will be gray background
+    for(var i = 1; i < 10; i++) {
+        var x = i + 8;
+        if(currentTime > x) {
+            $(`#row${i} textarea`).addClass("bg-secondary")
+        } else if(currentTime < x) {
+            $(`#row${i} textarea`).addClass("bg-info")
+        } else {
+            $(`#row${i} textarea`).addClass("bg-success")
+        }
     }
-    console.log(currentTime);
 };
 
 // Save button is clicked
 $(".savebtn").click(function(){
-    console.log("hello");
+    var taskText = $(this).siblings('textarea').val();
+    var taskTime = $(this).siblings('p').text();
+
+    console.log(taskText);
+    console.log(taskTime);
+
+    tasks.push({
+        text: taskText,
+        time: taskTime
+    })
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    console.log(tasks);
 });
 
-hourUpdate();
+var loadData = function() {
+    localStorage.getItem(tasks.text)
+    
+}
+
+var reset = function() { 
+    tasks = [];
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    location.reload();
+};
+
+setInterval(hourUpdate(), (1000*60));
