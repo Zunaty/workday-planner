d88a3b0b1760228530e2e522bec8f42cc059b0dd
Victor Perez <zunaty@gmail.com>
2021-07-18 16:16:18 -0600

    working on loading data from LS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,8 @@ var tasks = [];
 var currentTime = moment().format("H")
 var today = moment().format("LL");
 document.getElementById("currentDay").innerText = today;
-console.log(today);
-console.log(currentTime);
 
+// Checks the current time and updates the colors of each time block
 var hourUpdate = function() {
     for(var i = 1; i < 10; i++) {
         var x = i + 8;
@@ -16,6 +15,18 @@ var hourUpdate = function() {
             $(`#row${i} textarea`).addClass("bg-success")
         }
     }
+};
+
+// Reset the page
+var reset = function() { 
+    tasks = [];
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    location.reload();
+};
+
+// Populate data back into textarea
+function createData(text, time) {
+    $(`#row${time} textarea`).val(text);
 };
 
 // Save button is clicked
@@ -31,19 +42,28 @@ $(".savebtn").click(function(){
         time: taskTime
     })
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    console.log(tasks);
+    saveTasks();
 });
 
-var loadData = function() {
-    localStorage.getItem(tasks.text)
-    
-}
-
-var reset = function() { 
-    tasks = [];
+// Saving to tasks to local storage
+var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-    location.reload();
 };
 
-setInterval(hourUpdate(), (1000*60));
+// Loads the data from local storage to the tasks array
+var loadData = function() {
+    tasksLS = JSON.parse(localStorage.getItem("tasks"));
+
+    if(!tasksLS) {
+        tasks = [];
+    }
+
+    for(var i = 0; i < tasksLS.length; i++){
+        console.log(tasksLS[i]);
+        createData(tasksLS.text, tasksLS.time)
+        console.log(tasksLS.text)
+    }
+};
+
+loadData();
+setInterval(hourUpdate(), (1000*30));
